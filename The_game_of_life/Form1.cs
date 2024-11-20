8389d884793f0d744c5e76793adc0060bf3df0b7
AsuraMTG <eper.karma@gmail.com>
2024-11-20 09:30:16 +0100

    2024.11.20

--- The_game_of_life/Form1.cs
+++ The_game_of_life/Form1.cs
@@ -16,10 +16,40 @@ namespace The_game_of_life
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        public void rajzol(int x, int y)
+        {
+            Bitmap myBitmap = new Bitmap("600x600.bmp");
+            
+
+        }
+
+        public Bitmap myBitmap = new Bitmap("600x600.bmp");
+        public int x = 30;
+        public int y = 30;
+        public int[] pontok = new int[1000]; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            pictureBox1.Image = myBitmap;
+            Color c = myBitmap.GetPixel(30, 30);
+
+            myBitmap.SetPixel(x, y, Color.Red);
+            myBitmap.SetPixel(x, y + 1, Color.Red);
+            myBitmap.SetPixel(x, y - 1, Color.Red);
+            myBitmap.SetPixel(x + 1, y + 1, Color.Red);
+            myBitmap.SetPixel(x + 1, y, Color.Red);
+            myBitmap.SetPixel(x + 1, y - 1, Color.Red);
+            myBitmap.SetPixel(x - 1, y, Color.Red);
+            myBitmap.SetPixel(x - 1, y + 1, Color.Red);
+            myBitmap.SetPixel(x - 1, y - 1, Color.Red);
+
+            x += 2;
         }
     }
 }
